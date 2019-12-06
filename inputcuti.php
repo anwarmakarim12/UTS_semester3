63cdc6a412808f377579c15ce031a0727ff94c95
anwarmakarim12 <58603192+anwarmakarim12@users.noreply.github.com>
2019-12-06 23:40:58 +0700

    Membuat file input cuti untuk tugas UTS pemograman3

---
+++ inputcuti.php
@@ -0,0 +1,41 @@
+<?php
+//fungsi untuk menggabungkan file
+include("koneksi.php");
+if (isset($_POST['simpan'])) {
+	$query_input_cuti = "insert into t_cuti(nik,nama,jabatan,hakCuti)values
+	('" . $_POST['nik'] . "',
+	 '" . $_POST['nama'] . "',
+    '" . $_POST['jabatan'] . "',
+'" . $_POST['hakCuti'] . "')";
+	$proses_data = mysql_query($query_input_cuti);
+	if ($proses_data) {
+		echo 'Input Data Berhasil ';
+	} else {
+		echo mysql_error();
+	}
+}
+?>
+<form method="POST" action="">
+	<table>
+		<tr>
+			<td>Nik Karyawan</td>
+			<td><input name="nik" type="text"></td>
+		</tr>
+		<tr>
+			<td>Nama Karyawan</td>
+			<td><input name="nama" type="text"></td>
+		</tr>
+		<tr>
+			<td>Jabatan Karyawan</td>
+			<td><input name="jabatan" type="text"></td>
+		</tr>
+		<tr>
+			<td>Hak Cuti Karyawan</td>
+			<td><input name="hakCuti" type="number"></td>
+		</tr>
+		<tr>
+			<td><input name="simpan" type="submit"></td>
+		</tr>
+		<button><a href="tampilCuti.php">tampilkan data</a></button>
+	</table>
+</form>
